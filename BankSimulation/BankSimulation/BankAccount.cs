fa71989a680357db9826b58844d7704b2b09f06e
Charlie Splittstoser <char.split@gmail.com>
2017-03-14 19:52:20 -0700

    Test

---
+++ BankSimulation/BankSimulation/BankAccount.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSimulation
+{
+    class BankAccount
+    {
+
+        /* Constants */
+        const int SAVINGS = 0;
+        const int CHECKING = 1;
+        
+        /******** Constructors ********/
+        public BankAccount()
+        {
+            this.firstName = "N/A";
+            this.lastName = "N/A";
+            this.accountBalance[SAVINGS] = 0;
+            this.accountBalance[CHECKING] = 0;
+        }
+
+        public BankAccount(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.accountBalance[SAVINGS] = 0;
+            this.accountBalance[CHECKING] = 0;
+        }
+
+
+        /************************ Methods *********************************/
+        
+        /**********************************
+         * Function: generateAccountNumber
+         * Description: Checks latest account number and returns that number + 1
+         * PostCond: Assigns an account # to the current account objct in scope
+         *********************************/ 
+        public int generateAccountNumber()
+        {
+            int accountNumber = 0;
+            string path = @"../../accounts/latestAccountNumber.txt";
+
+            /* Read Latest Account Number */
+            using (StreamReader sr = File.OpenText(path))
+            {
+                accountNumber = Convert.ToInt32(sr.ReadLine()) + 1;
+            }
+
+            /* Overwrite Latest Account Number */
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(accountNumber);
+            }
+
+            this.accountNumber = accountNumber;
+
+                return accountNumber;
+        }
+
+        /**********************************
+         * Function: saveAccountData
+         * Description: creates a file names account# and saves info to it
+         * PostCond: Save first, last, account #, and account balances to the file
+         *********************************/
+        public void saveAccountData()
+        {
+            string path = @"../../accounts/accounts/" + this.accountNumber + ".txt";
+
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine("Account Number: " + this.accountNumber);
+                sw.WriteLine("Account Holder Name: " + this.firstName + " " + this.lastName);
+                sw.WriteLine("Savings: " + this.accountBalance[SAVINGS]);
+                sw.WriteLine("Checking: " + this.accountBalance[CHECKING]);
+            }
+        }
+
+
+        /****************************
+        * Funciton: handleAccountOptionsSelection
+        * Description: handles user selection for if they want to:
+        *              - Check balance
+        *              - Deposit
+        *              - Withdrawal
+        *              - Exit program
+        ***************************/
+        public void handleAccountOptions(int userSelection)
+        {
+            switch (userSelection)
+            {
+                /* Exit the program */
+                case 0:
+                    Console.WriteLine("The program will now exit. Thanks for doing business!");
+                    System.Environment.Exit(1107);
+                    break;
+                case 1:
+                    //Check balance
+                    Console.WriteLine("Check Balance");
+                    break;
+                case 2:
+                    // Make a deposit
+                    Console.WriteLine("Make a deposit");
+                    break;
+                case 3:
+                    // Make a withdrawal
+                    Console.WriteLine("Make a withdrawal");
+                    break;
+            }
+        }
+
+
+        /******** Getters & Setters ********/
+
+        public int getAccountNumber()
+        {
+            return this.accountNumber;
+        }
+
+
+        public string getAccountName(bool fullName)
+        {
+            if (fullName)
+                return this.firstName + " " + this.lastName;
+            else
+                return this.firstName;
+        }
+
+        /******** Data Members *************/
+        private double[] accountBalance = new double[2];
+        private string firstName;
+        private string lastName;
+        private int accountNumber;
+
+
+    }
+}
---
+++ BankSimulation/BankSimulation/UserInteraction.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankSimulation
+{
+    class UserInteraction
+    {
+
+        /*****************************
+         * Function: displayWelcomeMessage
+         * Description: Displays the welcome message for Splittstoser Banks
+         ****************************/
+       public static void displayWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Splittstoser Banks! How can we help you?\n");
+        }
+
+
+        /*****************************
+         * Function: displayUserOptions
+         * Description: Determines if user has an account or needs to create one
+         * PostCond: User selection option which will either exit, create account, or find account
+         ****************************/
+        public static int displayWelcomeOptions()
+        {
+            int userSelection = 0;
+            Console.WriteLine("1.) I would like to create a new bank account.");
+            Console.WriteLine("2.) I would like to access an existing bank account.");
+            Console.Write("Please enter 1 or 2 (Any other entry will exit): ");
+            try
+            {
+                userSelection = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e) { }
+
+            return userSelection;
+        }
+
+
+        /****************************
+         * Funciton: handleWelcomeOptions
+         * Description: handles logic based on if user has an account or needs to create one
+         * PostCond: BankAccount object instantiated from either existing or new bank account
+         ***************************/
+        public static BankAccount handleWelcomeOptions(int userSelection)
+        {
+            BankAccount b = new BankAccount();
+            switch (userSelection)
+            {
+                /* Exit the program */
+                case 0:
+                    Console.WriteLine("The program will now exit. Thanks for doing business!");
+                    System.Environment.Exit(1107);
+                    break;
+                case 1:
+                    b = createBankAccount();
+                    break;
+                case 2:
+                    b = searchForUserAccount();
+                    break;
+            }
+            return b;
+        }
+
+
+        /****************************
+         * Function: displayAccountOptions
+         * Description: Displays bank account options
+         *              - Check balance
+         *              - Deposit
+         *              - Withdrawal
+         *              - Exit program
+         ***************************/
+        public static void displayAccountOptions(BankAccount account)
+        {
+            int userSelection = 0;
+            Console.WriteLine("Welcome " + account.getAccountName(false) + "! How can we help you?");
+            Console.WriteLine("1.) Check my Balance");
+            Console.WriteLine("2.) Make a deposit");
+            Console.WriteLine("3.) Make a withdrawal");
+            Console.WriteLine("Please enter a number to select an option. Entering an invalid number will exit.");
+            try
+            {
+                userSelection = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e) { }
+
+            account.handleAccountOptions(userSelection);
+        }
+
+
+        /**************************************
+        * Function: searchForUserAccount
+        * Description: searches account list for an account that matches the account #
+        * PostCond: Bank account object with account #, first & last name of user
+        **************************************/
+        static BankAccount searchForUserAccount()
+        {
+            BankAccount account = new BankAccount();
+            //Add file processing & logic
+            return account;
+        }
+
+
+        /**************************************
+         * Function: createBankAccount
+         * Description: creates a new bank account for a new customer
+         * PostCond: Bank account object with account #, first & last name
+         **************************************/
+        static BankAccount createBankAccount()
+        {
+            string firstName;
+            string lastName;
+
+            Console.WriteLine("Thank you for your interest in Splittstoser Banks!");
+            Console.WriteLine("Please answer the following questions:");
+            Console.Write("What is your first name?: ");
+            firstName = Console.ReadLine();
+            Console.Write("What is your last name?: ");
+            lastName = Console.ReadLine();
+
+            BankAccount newBankAccount = new BankAccount(firstName, lastName);
+
+            newBankAccount.generateAccountNumber();
+            newBankAccount.saveAccountData();
+            
+
+                return newBankAccount;
+
+        }
+    }
+}
