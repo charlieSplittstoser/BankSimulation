b69c8ef022f7f6d0595214c8a3d6b3357bc50d99
Charlie Splittstoser <char.split@gmail.com>
2017-03-14 17:38:56 -0700

    Initial Commit

---
+++ BankSimulation/BankSimulation/Driver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSimulation
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test1");
+            BankAccount account = new BankAccount();
+            Console.WriteLine("Test2");
+            UserInteraction.displayWelcomeMessage();
+            account = UserInteraction.handleAccountSelection(UserInteraction.displayWelcomeOptions());
+            UserInteraction.displayAccountOptions(account);
+
+        }
+    }
+}
